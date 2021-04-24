71eaa43f63c36ab44d06de80aa4af556ae980a6a
Sam-Walters93 <stwalters314@gmail.com>
2021-04-24 10:06:06 -0500

    created files, link req NPM, wrote newEmployee function(nonoperational, requires outside functions)

--- app.js
+++ app.js
@@ -1,1 +1,94 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
 
+
+const employees = [];
+
+
+// const ganeratePage = require('')
+
+newEmployee = () => {
+    inquirer
+        .prompt([
+            {
+                message: "Enter team member's name.",
+                name: "name"
+            },
+            {
+                message: "Enter member's title",
+                choices: [
+                    "Team Lead",
+                    "Engineer",
+                    "intern",
+                ],
+                name: "title"
+            },
+            {
+                message: "Enter team member's ID",
+                name: "id"
+            },
+            {
+                message: "Enter team member's e-mail",
+                name: "email"
+            }
+        ])
+        .then(function({name, role, id, email}) {
+            let titleInfo = '';
+            if (role === "Engineer") {
+                titleInfo = 'GitHub UserName'
+            } 
+            if (role === "Intern") {
+                titleInfo = "University"
+            } else {
+                titleInfo = "Office Phone Extension"
+            } 
+            inquirer
+                .prompt([{
+                    message: `Enter Team Member's ${titleInfo}`,
+                    name: 'titleInfo'
+                },
+                {
+                    type: "list",
+                    message: "Would you like to add more team members?",
+                    choices: [
+                        "Yes",
+                        "No"
+                    ],
+                    name: "moreMembers"
+                }
+            ])
+                .then(function({titleInfo, moreMembers}) {
+                    let newMember;
+
+                    if (role === "Engineer") {
+                        newMember = new Engineer(name, id, email, titleInfo);
+                    }
+                    if (role === "Intern") {
+                        newMember = new Intern(name, id, email, titleInfo);
+                    } else {
+                        newMember = new Manager(name, id, email, titleInfo);
+                    }
+
+                    employees.push(newMember);
+                    generateCard(newMember)
+
+                    .then(function() {
+                        if (moreMembers === "Yes") {
+                            newEmployee();
+                        } else (
+                            generateHTML()
+                        )
+                    })
+                });
+        })
+};
+
+
+
+
+
+
+// fs.writeFile('index.html', generatePage(   ) , err => {
+//     if (err) throw err;
+  
+// });
