540dfe5c6fef5b9dd37d8a1e802b060913d4cab9
Sam-Walters93 <stwalters314@gmail.com>
2021-04-24 11:23:28 -0500

    refactored university/github prompt

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
 const employees = [];
 
 
 // const ganeratePage = require('')
 
-newEmployee = () => {
-    inquirer
-        .prompt([
+function newEmployee() {
+    inquirer.prompt([
             {
                 message: "Enter team member's name.",
                 name: "name"
             },
             {
-                message: "Enter member's title",
+                type: "list",
+                message: "Enter team member's role",
                 choices: [
-                    "Team Lead",
                     "Engineer",
-                    "intern",
+                    "Intern",
+                    "Manager"
                 ],
-                name: "title"
+                name: "role"
             },
             {
                 message: "Enter team member's ID",
@@ -30,22 +32,26 @@ newEmployee = () => {
             {
                 message: "Enter team member's e-mail",
                 name: "email"
-            }
-        ])
-        .then(function({name, role, id, email}) {
-            let titleInfo = '';
-            if (role === "Engineer") {
-                titleInfo = 'GitHub UserName'
-            } 
-            if (role === "Intern") {
-                titleInfo = "University"
-            } else {
-                titleInfo = "Office Phone Extension"
-            } 
-            inquirer
-                .prompt([{
-                    message: `Enter Team Member's ${titleInfo}`,
-                    name: 'titleInfo'
+            }])
+            .then(function(answers) {
+                let roleInfo;
+                
+                switch(answers.role) {
+                    case "Engineer":
+                        roleInfo = "GitHub UserName";
+                        break;
+                    case "Intern":
+                        roleInfo = "University";
+                        break;
+                    case "Manager":
+                        roleInfo = "Office Phone Number";
+                        break;
+                }   
+                
+            inquirer.prompt([
+                {
+                    message: `Enter Team Member's ${roleInfo}`,
+                    name: 'roleInfo'
                 },
                 {
                     type: "list",
@@ -56,17 +62,16 @@ newEmployee = () => {
                     ],
                     name: "moreMembers"
                 }
-            ])
-                .then(function({titleInfo, moreMembers}) {
+            ]).then(function({roleInfo, moreMembers}) {
                     let newMember;
 
                     if (role === "Engineer") {
-                        newMember = new Engineer(name, id, email, titleInfo);
+                        newMember = new Engineer(name, id, email, roleInfo);
                     }
                     if (role === "Intern") {
-                        newMember = new Intern(name, id, email, titleInfo);
+                        newMember = new Intern(name, id, email, roleInfo);
                     } else {
-                        newMember = new Manager(name, id, email, titleInfo);
+                        newMember = new Manager(name, id, email, roleInfo);
                     }
 
                     employees.push(newMember);
@@ -86,7 +91,7 @@ newEmployee = () => {
 
 
 
-
+newEmployee();
 
 // fs.writeFile('index.html', generatePage(   ) , err => {
 //     if (err) throw err;
