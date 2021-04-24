2f53fa9fb90908f4ae9feb58b8258606251e7e8c
Sam-Walters93 <stwalters314@gmail.com>
2021-04-24 12:15:53 -0500

    completed tests for employee contructor

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,64 @@
+const { test, expect } = require('@jest/globals');
+const Employee = require('../lib/Employee');
+
+test('check if employee constructor creates an object', () => {
+    const testEmp = new Employee;
+
+    expect(typeof testEmp).toBe('object');
+});
+
+test('employee constructor set name', () => {
+    const name = 'Tessa';
+    const testEmp = new Employee(name);
+
+    expect(testEmp.name).toBe(name);
+});
+
+test('will constructor function return employee name', () => {
+    const name = 'Tessa';
+    const testEmp = new Employee(name);
+
+    expect(testEmp.getName()).toBe(name);
+});
+
+test('employee constructor set employee id', () => {
+    const id = '8888';
+    const testEmp = new Employee('foo', id);
+
+    expect(testEmp.id).toBe(id);
+});
+
+test('will constructor function return employee id', () => {
+    const id = '8888';
+    const testEmp = new Employee('foo', id);
+
+    expect(testEmp.getId()).toBe(id);
+});
+
+test('employee constructor set employee email', () => {
+    const email = '8888';
+    const testEmp = new Employee('foo', 'Foo', email);
+
+    expect(testEmp.email).toBe(email);
+});
+
+test('will constructor function return employee email', () => {
+    const email = '8888';
+    const testEmp = new Employee('foo', 'Foo', email);
+
+    expect(testEmp.getEmail()).toBe(email);
+});
+
+test('employee constructor set employee role', () => {
+    const role = '8888';
+    const testEmp = new Employee('foo', 'Foo', 'Foo', role);
+
+    expect(testEmp.role).toBe(role);
+});
+
+test('will constructor function return employee role', () => {
+    const role = '8888';
+    const testEmp = new Employee('foo', 'Foo', 'Foo', role);
+
+    expect(testEmp.getRole()).toBe(role);
+});
