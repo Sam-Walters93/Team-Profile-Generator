e2fff9e577e193a7d00c7383a75e4996da702a45
Sam <stwalters314@gmail.com>
2021-04-24 17:57:36 -0500

    small refactoring changes

--- index.js
+++ index.js
@@ -9,81 +9,84 @@ const employees = [];
 
 function newEmployee() {
     inquirer.prompt([
+        {
+            message: "Enter team member's name",
+            name: "name"
+        },
+        {
+            type: "list",
+            message: "Enter team member's role",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Manager"
+            ],
+            name: "role"
+        },
+        {
+            message: "Enter team member's ID",
+            name: "id"
+        },
+        {
+            message: "Enter team member's e-mail",
+            name: "email"
+        }
+    ])
+    .then(function({name, role, id, email}) {
+        let roleInfo;
+            
+        switch(role) {
+            case "Engineer":
+                roleInfo = "GitHub UserName";
+                break;
+            case "Intern":
+                roleInfo = "University";
+                break;
+            case "Manager":
+                roleInfo = "Office Phone Number";
+                break;
+        }   
+            
+        inquirer.prompt([
             {
-                message: "Enter team member's name",
-                name: "name"
+                message: `Enter Team Member's ${roleInfo}`,
+                name: 'roleInfo'
             },
             {
                 type: "list",
-                message: "Enter team member's role",
+                message: "Would you like to add more team members?",
                 choices: [
-                    "Engineer",
-                    "Intern",
-                    "Manager"
+                    "Yes",
+                    "No"
                 ],
-                name: "role"
-            },
-            {
-                message: "Enter team member's ID",
-                name: "id"
-            },
-            {
-                message: "Enter team member's e-mail",
-                name: "email"
-            }])
-            .then(function({name, role, id, email}) {
-                let roleInfo;
-                
-                switch(role) {
-                    case "Engineer":
-                        roleInfo = "GitHub UserName";
-                        break;
-                    case "Intern":
-                        roleInfo = "University";
-                        break;
-                    case "Manager":
-                        roleInfo = "Office Phone Number";
-                        break;
-                }   
-                
-            inquirer.prompt([
-                {
-                    message: `Enter Team Member's ${roleInfo}`,
-                    name: 'roleInfo'
-                },
-                {
-                    type: "list",
-                    message: "Would you like to add more team members?",
-                    choices: [
-                        "Yes",
-                        "No"
-                    ],
-                    name: "moreMembers"
-                }
-            ]).then(function({roleInfo, moreMembers}) {
-                    let newMember;
+                name: "moreMembers"
+            }
+        ])
+        .then(function({roleInfo, moreMembers}) {
+            let newMember;
 
-                    if (role === "Engineer") {
-                        newMember = new Engineer(name, role, id, email, roleInfo);
-                    }
-                    if (role === "Intern") {
-                        newMember = new Intern(name, role, id, email, roleInfo);
-                    }  
-                    if (role === "Manager") {
-                        newMember = new Manager(name, role, id, email, roleInfo);
-                    }
+            if (role === "Engineer") {
+                newMember = new Engineer(name, role, id, email, roleInfo);
+            }
+            if (role === "Intern") {
+                newMember = new Intern(name, role, id, email, roleInfo);
+            }  
+            if (role === "Manager") {
+                newMember = new Manager(name, role, id, email, roleInfo);
+            }
 
-                    employees.push(newMember);
-                    generateCard(newMember)
-                    .then(function() {
-                        if (moreMembers === "Yes") {
-                            newEmployee();
-                        } else (
-                            completeFile()
-                        )
-                    })
-                });
-        })
+            employees.push(newMember);
+            generateCard(newMember)
+
+            .then(function() {
+                if (moreMembers === "Yes") {
+                    newEmployee();
+                } else (
+                    completeFile()
+                )
+            })
+        });
+    })
 };
 
 function createFile() {
@@ -94,13 +97,7 @@ function createFile() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link
-        rel="stylesheet"
-        href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"
-        integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V"
-        crossorigin="anonymous"/>
-        <script type="text/javascript" src="./index.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous"/>
         <title>Team Profile</title>
     </head>
     <body>
@@ -181,7 +178,8 @@ function generateCard(member) {
             };
             return resolve();
         });
-    }).catch(err => {
+    })
+    .catch(err => {
         console.log(err);
     });
 };
